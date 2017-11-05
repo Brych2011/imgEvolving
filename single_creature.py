40b760c85c707ecaedd188856014dd72c6102446
Brych2011 <bryszeg@gmail.com>
2017-11-05 20:09:35 +0100

    Added comments

--- single_creature.py
+++ single_creature.py
@@ -10,29 +10,30 @@ IMAGE.thumbnail((90,60))
 IMAGE.save('mona_lisa_even_smaller.jpg', 'JPEG')
 TARGET = np.array(IMAGE)
 """
-pg_im = pygame.image.load('mona_lisa_even_smaller.jpg')
-TARGET = pygame.surfarray.array3d(pg_im).astype('int16')
+pg_im = pygame.image.load('mona_lisa_even_smaller.jpg')   # #load target image
+TARGET = pygame.surfarray.array3d(pg_im).astype('int16')  # #convert to int16 array
 SIZE = pg_im.get_size()
 SHAPE = TARGET.shape
 print(SIZE
       )
 print(TARGET.shape)
-CIRCLES = 40
-ACCURACY = 1
+CIRCLES = 70
+ACCURACY = 1  # #obsolete(?)
 
 
 def genome_to_array(genome):
     pgim = pygame.Surface(SIZE, pygame.SRCALPHA)
     for circle in genome:
-        new_im = pygame.Surface((circle[2] * 2, circle[2] * 2), pygame.SRCALPHA)
+        new_im = pygame.Surface((circle[2] * 2, circle[2] * 2), pygame.SRCALPHA)  #create a surface of size of the circle
         color = circle[0].copy()
-        color.append(circle[3])
-        pygame.draw.circle(new_im, color, (circle[2], circle[2]), circle[2])
-        pgim.blit(new_im, [circle[1][i] - circle[2] for i in range(2)])
-    return pygame.surfarray.array3d(pgim).astype('int16')
+        color.append(circle[3])  # #add opacity to the color argument
+        pygame.draw.circle(new_im, color, (circle[2], circle[2]), circle[2])  # #draw circle in the middle
+        pgim.blit(new_im, [circle[1][i] - circle[2] for i in range(2)])  # #blit onto main surface
+    return pygame.surfarray.array3d(pgim).astype('int16')  # #return as int16 array
 
 
 def random_genome():
+    """creates a random genome"""
     result = []
     for i in range(CIRCLES):
         pos = [random.randint(0, SIZE[0]-1), random.randint(0, SIZE[1]-1)]
@@ -44,40 +45,16 @@ def random_genome():
 
 
 def check_fitness(genome):
+    """returns genome's fitness counted by difference in rgb values"""
     genome_array = genome_to_array(genome)
     fitness = (sum(abs(TARGET - genome_array).flat) ** 2) * -1
+    # #squared for rewarding better creatures, difference is a negative trait, hence * -1
     return fitness
-
-
-def check_fitness3(genome):
-    genome_array = genome_to_array(genome)
-    difference = genome_array - TARGET
-    abs_difference = abs(difference)
-    sum_difference = sum(abs_difference.flat)
-    print(type(sum_difference))
-    fitness = sum_difference ** 2 * -1
-    return fitness
-
-
-def check_fitness2(genome):
-    genome_array = genome_to_array(genome)
-    w, h, d = TARGET.shape
-    target2 = TARGET.reshape(w * h, d)
-    genome2 = genome_array.reshape(w * h, d)
-    fitness = (sum(abs(genome2 - target2).flat) ** 2) * -1
-    return fitness
-
-
-def show_fitness_errors(genome):
-    genome_array = genome_to_array(genome)
-    difference1 = TARGET - genome_array
-    difference2 = genome_array - TARGET
-    return difference1, difference2, TARGET
 
 
 def draw_creature(genome):
     pgim = pygame.Surface((SIZE[0], SIZE[1]), pygame.SRCALPHA)
-    for circle in genome:
+    for circle in genome:  # #same as in genome_to_array
         new_im = pygame.Surface((circle[2] * 2, circle[2] * 2), pygame.SRCALPHA)
         color = circle[0].copy()
         color.append(circle[3])
@@ -89,41 +66,41 @@ def draw_creature(genome):
 
 
 def mutate(start_genome):
-    genome = deepcopy(start_genome)
+    genome = deepcopy(start_genome)  # #deepcopy is necessary (apparently)
     repeat = True
     while repeat:
-        choice = random.randint(1, 5)
-        if choice == 1:
+        choice = random.randint(1, 5)  # #choose a random mutation
+        if choice == 1:  # #redefine color
             genome[random.randint(0, CIRCLES - 1)][0] = [random.randint(0, 255) for i in range(3)]
-        elif choice == 2:
+        elif choice == 2:  # #redefine position
             genome[random.randint(0, CIRCLES - 1)][1] = [random.randint(0, SIZE[0] - 1),
                                                          random.randint(0, SIZE[1] - 1)]
-        elif choice == 3:
+        elif choice == 3:  # #redefine radius
             genome[random.randint(0, CIRCLES - 1)][2] = random.randint(1,30)
-        elif choice == 4:
+        elif choice == 4:  # #swap two circles on z axis
             index1 = random.randint(0, CIRCLES-1)
             index2 = random.randint(0, CIRCLES-1)
             while index1 == index2:
-                index2 = random.randint(0, CIRCLES - 1)
+                index2 = random.randint(0, CIRCLES - 1)  # #generate two random indices
             temp = genome[index1]
-            genome[index1] = genome[index2]
+            genome[index1] = genome[index2]  # #swap them
             genome[index2] = temp
-        elif choice == 5:
+        elif choice == 5:  # #redefine opacity
             genome[random.randint(0, CIRCLES - 1)][3] = random.randint(0,255)
-        repeat = not random.randint(0, 9)
+        repeat = not random.randint(0, 9)  # #have 10% chance for another mutation
     return genome
 
 
 if __name__ == '__main__':
-    creature = random_genome()
+    creature = random_genome()  # #generate starting genome
 
     improvements = 0
     staring_fitness = check_fitness(creature)
-    for i in range(100000):
+    for i in range(300000):
         if i % 500 == 0:
-            print(i, check_fitness(creature))
-        kid = mutate(creature)
-        if check_fitness(creature) < check_fitness(kid):
+            print(i, check_fitness(creature))  # #check progress every 500 gens
+        kid = mutate(creature)  # #create a mutated kid
+        if check_fitness(creature) < check_fitness(kid):  # #replace genome if mutation if beneficial
             creature = kid
             improvements += 1
     # #IMAGE.show()
@@ -144,5 +121,6 @@ if __name__ == '__main__':
     draw_creature(test)
     draw_creature(creature)
     """
+
 
 
