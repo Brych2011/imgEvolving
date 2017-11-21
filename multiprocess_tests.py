8d5e2bace0dde2c5b7889c69f401202e7757f6db
Brych2011 <bryszeg@gmail.com>
2017-11-21 20:48:39 +0100

    Some testing

--- multiprocess_tests.py
+++ multiprocess_tests.py
@@ -11,10 +11,10 @@ def call_mutate(creature):
 
 
 if __name__ == '__main__':
-    genome_list = [Genome(100) for i in range(2000)]
+    genome_list = [Genome(100) for i in range(5000)]
+    p = Pool(5)
     t1 = time.time()
-    p = Pool(70)
-    genome_list = p.map(call_mutate, genome_list)
+    genome_list = list(p.map(call_mutate, genome_list))
     time_multi = time.time() - t1
     t2 = time.time()
     for creature in genome_list:
@@ -22,8 +22,11 @@ if __name__ == '__main__':
         creature.update_array()
         creature.update_fitness()
     time_single = time.time() - t2
+    t3 = time.time()
+    genome_list = list(p.imap(call_mutate, genome_list, chunksize=1000))
+    time_imap = time.time() - t3
 
-    print(time_multi, time_single)
+    print(time_multi, time_single, time_imap)
 
 
 
